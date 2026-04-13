fbc54b476e403a5d6800f1e4ad9a94d69eea19be
labintsevartur <unlmtddd@gmail.com>
2026-04-13 15:31:17 +0200

    UDPSender-Klasse zum Senden von Audiodaten über UDP hinzugefügt
    UDPReceiver-Klasse zum Empfangen von UDP-Paketen hinzugefügt
    
    AudioSender umstrukturiert um UDPSender zu verwenden
    
    AudioReceiver umstrukturiert um UDPReceiver zu verwenden

---
+++ src/VideoCall/VideoCall/UDPReciever.java
@@ -0,0 +1,36 @@
+package VideoCall;
+import java.net.*;
+
+public class UDPReciever
+
+{
+
+
+        private DatagramSocket socket ;
+        private byte []  buffer ;
+
+
+        public UDPReciever(int port ) throws Exception
+        {
+            this.socket = new DatagramSocket(port) ;
+            this.buffer = new byte[1024];
+        }
+
+        public byte[] receiver () throws Exception
+        {
+            DatagramPacket packet = new DatagramPacket(buffer,buffer.length);
+            socket.receive(packet);
+            byte[] data = new byte [packet.getLength()];
+            System.arraycopy(packet.getData(),0,data,0,packet.getLength());
+            return data;
+
+         }
+
+         public void close()
+         {
+             socket.close();
+
+         }
+
+
+}
