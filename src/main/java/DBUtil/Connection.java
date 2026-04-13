527f5126398ac841c2066e9054464aaba329a35f
Domi2445 <austdominik@student.dv-schulen.de>
2026-04-13 14:30:17 +0200

    vorbeireitung auf hibernate

---
+++ src/main/java/DBUtil/Connection.java
@@ -0,0 +1,24 @@
+package DBUtil;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Connection {
+	private static final String PERSISTENCE_UNIT_NAME = "chat-oracle";
+	private static final EntityManagerFactory ENTITY_MANAGER_FACTORY =
+			Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+
+	private Connection() {
+	}
+
+	public static EntityManager createEntityManager() {
+		return ENTITY_MANAGER_FACTORY.createEntityManager();
+	}
+
+	public static void closeFactory() {
+		if (ENTITY_MANAGER_FACTORY.isOpen()) {
+			ENTITY_MANAGER_FACTORY.close();
+		}
+	}
+}
