b0674811248916cc8f1e7fde4372781d55050c28
Swarkin <102416174+Swarkin@users.noreply.github.com>
2026-04-15 11:26:27 +0200

    Emojis fixen

--- src/main/java/Util/Emoji/EmojiMessage.java
+++ src/main/java/Util/Emoji/EmojiMessage.java
@@ -1,6 +1,6 @@
 package Util.Emoji;
 
-import Util.Message.Message;
+import Util.Network.Messages.Message;
 import Util.User;
 
 public class EmojiMessage extends Message {
@@ -14,20 +14,5 @@ public class EmojiMessage extends Message {
 
     public String getEmoji() {
         return emoji;
-    }
-
-    @Override
-    public String getContent() {
-        return emoji;
-    }
-
-    @Override
-    public String getDisplayText() {
-        return emoji;
-    }
-
-    @Override
-    public void send() {
-        System.out.println(getSender().getUsername() + " sendet Emoji: " + emoji);
     }
 }
