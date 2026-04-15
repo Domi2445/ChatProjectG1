8de49f42444017b20beb44d7df3c80b7491b6fb7
Swarkin <102416174+Swarkin@users.noreply.github.com>
2026-04-15 11:26:42 +0200

    Serializable

--- src/main/java/Util/Network/Messages/Message.java
+++ src/main/java/Util/Network/Messages/Message.java
@@ -3,8 +3,10 @@ package Util.Network.Messages;
 import Util.Network.Packet;
 import Util.User;
 
+import java.io.Serializable;
+
 // Oberklasse für alle Benutzernachrichten
-public abstract class Message extends Packet {
+public abstract class Message extends Packet implements Serializable {
     private final User sender;
 
     public Message(User sender) {
--- src/main/java/Util/Network/Packet.java
+++ src/main/java/Util/Network/Packet.java
@@ -1,4 +1,6 @@
 package Util.Network;
 
+import java.io.Serializable;
+
 // Oberklasse für alle Pakete, die über das Netzwerk gesendet werden
-public abstract class Packet {}
+public abstract class Packet implements Serializable {}
