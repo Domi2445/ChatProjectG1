e2701dfc241815fa124a79f1d602939ca02598fa
TillKloss <tilleliaskloss@gmail.com>
2026-04-17 08:35:48 +0200

    BCryptWrapper

---
+++ src/main/java/Util/BCryptWrapper.java
@@ -0,0 +1,26 @@
+package Util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class BCryptWrapper {
+    public static String hash(String plainText)
+    {
+        if (plainText == null || plainText.isBlank()) {
+            throw new IllegalArgumentException("BCryptWrapper - Password darf nicht leer sein.");
+        }
+        return BCrypt.hashpw(plainText, BCrypt.gensalt(12));
+    }
+
+    public static boolean validate(String plainText, String hashedPwd)
+    {
+        if (plainText == null || plainText.isBlank() || hashedPwd == null || hashedPwd.isBlank()) return false;
+
+        try {
+            return BCrypt.checkpw(plainText, hashedPwd);
+        } catch (Exception e)
+        {
+            System.out.println("BCrpytWrapper - "+e);
+        }
+        return false;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.chatproject {
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.base;
+    requires jbcrypt;
 
     opens Client to javafx.graphics, javafx.base;
     exports Client;
