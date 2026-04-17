2df3dbf90f9acba27e1f3efc4294b04294e8de0b
TillKloss <tilleliaskloss@gmail.com>
2026-04-17 09:28:49 +0200

    String profilePicturePath -> long profilePictureUUID

--- src/main/java/User/Model/User.java
+++ src/main/java/User/Model/User.java
@@ -6,16 +6,16 @@ public class User {
     private String passwordHash;
     private String statusMessage;
     private String profileDescription;
-    private String profilePicturePath;
+    private long profilePictureUUID;
     private ContactData contactData;
 
-    public User(String username, String displayname, String passwordHash, String statusMessage, String profileDescription, String profilePicturePath, ContactData contactData) {
+    public User(String username, String displayname, String passwordHash, String statusMessage, String profileDescription, long profilePictureUUID, ContactData contactData) {
         this.username = username;
         this.displayname = displayname;
         this.passwordHash = passwordHash;
         this.statusMessage = statusMessage;
         this.profileDescription = profileDescription;
-        this.profilePicturePath = profilePicturePath;
+        this.profilePictureUUID = profilePictureUUID;
         this.contactData = contactData;
     }
 
@@ -59,12 +59,12 @@ public class User {
         this.profileDescription = profileDescription;
     }
 
-    public String getProfilePicturePath() {
-        return profilePicturePath;
+    public long getProfilePictureUUID() {
+        return profilePictureUUID;
     }
 
-    public void setProfilePicturePath(String profilePicturePath) {
-        this.profilePicturePath = profilePicturePath;
+    public void setProfilePictureUUI(long profilePictureUUID) {
+        this.profilePictureUUID = profilePictureUUID;
     }
 
     public ContactData getContactData() {
