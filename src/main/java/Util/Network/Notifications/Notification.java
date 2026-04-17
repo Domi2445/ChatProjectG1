72ee6105f673121c29e17f571b04ba6bced260f8
Swarkin <102416174+Swarkin@users.noreply.github.com>
2026-04-17 08:26:19 +0200

    Fix abstract Notification

--- src/main/java/Util/Network/Notifications/Notification.java
+++ src/main/java/Util/Network/Notifications/Notification.java
@@ -3,4 +3,4 @@ package Util.Network.Notifications;
 import Util.Network.Packet;
 
 // Oberklasse für alle Datenpakete, die keine Benutzernachrichten sind
-public class Notification extends Packet {}
+public abstract class Notification extends Packet {}
