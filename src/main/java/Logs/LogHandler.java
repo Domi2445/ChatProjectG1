c69e1fe9845945783f1fadbf013b31796a9e6a4a
TillKloss <tilleliaskloss@gmail.com>
2026-04-20 13:51:18 +0200

    [WIP] Logging

--- src/main/java/Logs/LogHandler.java
+++ src/main/java/Logs/LogHandler.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 public class LogHandler {
-	private static Path path = Path.of("src/main/java/Logs/log.txt");
+	private static Path path = Path.of("Logs/log.txt");
 	public static void createLogEntry(String enterCurrentClassHere, String logMessage) {
 		try (BufferedWriter writer = new BufferedWriter(new FileWriter(path.toString(), true))) {
 			writer.write(new LogEntry(enterCurrentClassHere, logMessage).toString());
