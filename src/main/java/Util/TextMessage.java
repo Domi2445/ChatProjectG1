35650953290fbdc1a52df67450e79b555831d616
Marian <willmsmarian@student.dv-schulen.de>
2026-04-22 09:52:24 +0200

    Textnachrichten können bearbeitet und gelöscht werden. beim bearbeiten wird angezeigt in kursiv, dass die nachricht bearbeitet werden. wenn eine nachricht gelöscht wurde kommt ein hinweis das die nachricht gelöscht wurde aber diesen kann man nicht bearbeiten oder löschen.

--- src/main/java/Util/TextMessage.java
+++ src/main/java/Util/TextMessage.java
@@ -4,13 +4,31 @@ import User.Model.User;
 
 public class TextMessage extends Message {
 	private final String content;
+	private String editedContent;
+	private final long id;
+	private boolean isEdited;
 
 	public TextMessage(User sender, String content) {
 		super(sender);
 		this.content = content;
+		this.id = System.currentTimeMillis() + (long)(Math.random() * 1000);
+		this.isEdited = false;
 	}
 
 	public String getContent() {
-		return content;
+		return editedContent != null ? editedContent : content;
+	}
+
+	public void setEditedContent(String editedContent) {
+		this.editedContent = editedContent;
+		this.isEdited = true;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public boolean isEdited() {
+		return isEdited;
 	}
 }
