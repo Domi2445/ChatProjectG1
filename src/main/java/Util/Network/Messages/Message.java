723091c4a9f935d6bced88cfc7df01a109b387b1
dysum24 <sumnerdylan@student.dv-schulen.de>
2026-04-24 08:27:20 +0200

    add groupid to Message class

--- src/main/java/Util/Network/Messages/Message.java
+++ src/main/java/Util/Network/Messages/Message.java
@@ -3,13 +3,20 @@ package Util.Network.Messages;
 import Util.Network.Packet;
 import User.Model.User;
 
+import java.util.UUID;
+
 /// Oberklasse für alle Benutzernachrichten
 public abstract class Message extends Packet {
     private final User sender;
+	private UUID groupId;
 
-    public Message(User sender) {
-        this.sender = sender;
-    }
+	public Message(User sender)
+	{
+		this.sender = sender;
+		this.groupId = null;
+	}
 
     public User getSender() { return sender; }
+	public UUID getGroupId() { return groupId; }
+	public void setGroupId(UUID groupId) { this.groupId = groupId; }
 }
